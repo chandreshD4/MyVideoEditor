cadd2802cabbf346a4d0241304ad59c51dde8b54
chandreshD4 <rajputchandresh163pc1@gmail.com>
2026-09-19 13:42:38 +0530

    Add video picker

--- app/src/main/java/com/myvideoeditor/MainActivity.kt
+++ app/src/main/java/com/myvideoeditor/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.myvideoeditor
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
 
 class MainActivity : Activity() {
+
+    private val PICK_VIDEO = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val createButton = findViewById<Button>(R.id.createNewVideo)
+
+        createButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                type = "video/*"
+            }
+
+            startActivityForResult(intent, PICK_VIDEO)
+        }
+    }
+
+    @Deprecated("Deprecated in Android API")
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_VIDEO && resultCode == RESULT_OK) {
+            val videoUri = data?.data
+
+            if (videoUri != null) {
+                // अगला चरण: वीडियो को Editor Screen में दिखाएँगे
+            }
+        }
     }
 }
